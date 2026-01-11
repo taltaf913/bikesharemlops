92d26a8c834961eeef696803c43885f64d5d26ac
taltaf913 <taltaf913@gmail.com>
2026-01-11 19:40:28 +0530

    testadddd

--- tests/test_features.py
+++ tests/test_features.py
@@ -16,12 +16,12 @@ from bikeshare_model.processing.features import age_col_tfr
 def test_age_variable_transformer(sample_input_data):
     # Given
     transformer = age_col_tfr(
-        variables=config.model_config.age_var,  # cabin
+        variables=config.model_config.yr_var,  # cabin
     )
-    assert np.isnan(sample_input_data.loc[709,'Age'])
+    assert np.isnan(sample_input_data.loc[709,'yr'])  # ensure precondition
 
     # When
     subject = transformer.fit(sample_input_data).transform(sample_input_data)
 
     # Then
-    assert subject.loc[709,'Age'] == 21
+    assert subject.loc[709,'yr'] == 2001
